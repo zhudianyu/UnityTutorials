4501fd547a3f739a524b8f9b7cae6607a03d4a97
zhudianyu.zqgame <1462415060@qq.com>
2017-09-20 09:36:17 +0800

    初步实现曲线 用10个直线实现

---
+++ Assets/Scripts/CurvesAndSplines/Bezier.cs
@@ -0,0 +1,22 @@
+﻿
+//*************************************************************************
+//	创建日期:	2017/9/20 星期三 9:23:48
+//	文件名称:	Bezier
+//   创 建 人:   zhudianyu	
+//	版权所有:	zhudianyu
+//	说    明:	
+//*************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class Bezier
+{
+    public static Vector3 GetPoint(Vector3 p0,Vector3 p1,Vector3 p2,float t)
+    {
+        return Vector3.Lerp(Vector3.Lerp(p0,p1,t), Vector3.Lerp(p1,p2,t), t);
+    }
+}
